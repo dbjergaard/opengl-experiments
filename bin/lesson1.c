e89f989f90d061d7c834d23935ecfa55af55dabb
David Bjergaard <dbjergaard@gmail.com>
2016-02-29 08:21:41 -0500

    Latest code

--- bin/lesson1.c
+++ bin/lesson1.c
@@ -1,17 +1,18 @@
 #include "stdlib.h"
 #include "bitmap.h"
 #include "pngwriter.h"
+#include "render.h"
 
 int main(int argc, const char* argv[]){
   bitmap_t image;
   image.width = 100;
   image.height = 100;
   image.pixels = calloc(sizeof(pixel_t),image.width*image.height);
-  pixel_t red;
-  red.red=255;
-  red.green=0;
-  red.blue=0;
-  set_pixel(&image,52,41,&red);
-  save_png_to_file(&image,"output.png");
+  pixel_t red={255,0,0};
+  pixel_t white={255,255,255};
+  line(&image,13, 20, 80, 40, &white); 
+  line(&image,20, 13, 40, 80, &red); 
+  line(&image,80, 40, 13, 20, &red);
+  save_png_to_file(&image,"lesson1.png");
   return 0;
 }
